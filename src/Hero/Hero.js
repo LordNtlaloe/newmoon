7f84538b618783c02e457228957cb15f37a6c8c0
LordNtlaloe <ntlal0e182@gmail.com>
2023-03-11 17:53:02 +0200

    slider update

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -6,7 +6,7 @@ export default function Hero(){
         <div className="hero">
             <div className="hero-content-span">
                 <span>Join Us Now!</span>
-                <h3>Make The Best Decision Of Your Today</h3>
+                <h3>Make The Best Decision Of Life Your Today</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo assumenda laboriosam delectus, quaerat earum eaque velit explicabo deleniti, voluptates dolor temporibus animi. Perferendis et corporis porro libero illum dolore aspernatur.</p>
                 <a href="#contact" className="hero-btn">Join Us</a>
             </div>
