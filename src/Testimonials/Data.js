5c6e611156714875ef3b4cd390bd9fa0d0002c1c
LordNtlaloe <ntlal0e182@gmail.com>
2023-04-07 18:58:39 +0200

    Some update

---
+++ src/Testimonials/Data.js
@@ -0,0 +1,27 @@
+import Image1 from '../Assets/Profile01.jpg';
+import Image2 from '../Assets/Profile02.jpg';
+import Image3 from '../Assets/Profile03.jpg';
+
+
+export const Data = [
+    {
+        id: 1,
+        image: Image1,
+        title: "User One",
+        program: "Weight Loss Therapy",
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit dolores natus dolorum repudiandae sint est, aliquam saepe eaque consequuntur provident mollitia nemo sunt cupiditate. Harum iure sapiente dolore velit similique."
+    }, {
+        id: 2,
+        image: Image2,
+        title: "User Two",
+        program: "Weight Loss Therapy",
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit dolores natus dolorum repudiandae sint est, aliquam saepe eaque consequuntur provident mollitia nemo sunt cupiditate. Harum iure sapiente dolore velit similique."
+    }, {
+        id: 3,
+        image: Image3,
+        title: "User Three",
+        program: "Weight Loss Therapy",
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit dolores natus dolorum repudiandae sint est, aliquam saepe eaque consequuntur provident mollitia nemo sunt cupiditate. Harum iure sapiente dolore velit similique."
+    }
+
+]
--- src/Testimonials/Testimonials.js
+++ src/Testimonials/Testimonials.js
@@ -1,121 +1,45 @@
-import React from 'react';
-import {Swiper,SwiperSlide} from 'swiper/react';
-import 'swiper/css/bundle';
-import 'swiper/css/pagination';
-import {Pagination, Autoplay} from "swiper";
+import React from "react";
 import './testimonials.css';
-import profile1 from '../Assets/Profile01.jpg';
-import profile2 from '../Assets/Profile02.jpg';
-import profile3 from '../Assets/Profile03.jpg';
-import imgBefore1 from '../Assets/charlene-before.jpg';
-import imgAfter1 from '../Assets/charlene-after.jpg';
-import imgBefore2 from '../Assets/jade_socoby_before.jpg';
-import imgAfter2 from '../Assets/jade_socoby_after.jpg';
+import { Data } from "./Data";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination, Autoplay } from "swiper";
 
-export default function Testimonials(){
-    return(
-        <section className="testimonials" id="testimonials">
+
+export default function Testimonials() {
+    return (
+        <section className="tesimonials section container" id="testimonials">
             <div className="section-title">
-                <h2>Testimonials</h2>
+                <h2>Testimonials</h2>     
             </div>
-            <div className="testimonial-content">
-                <div className="text">
-                    <h3>Testimonials</h3>
-                    <p className="title">What Our Clients Have To Say</p>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita, tenetur nesciunt quam ad ratione nulla alias veritatis maxime, illum sit rerum amet incidunt soluta hic!</p>
-                    <a href="#" className="testimonial-button">Read More...</a>
-                </div>
-                <Swiper className="client-testimonials mySwiper" autoplay={{ delay: 2000, disableOnInteraction: false}} slidesPerView={2} loop={true} grabCursor={false} spaceBetween={24} pagination={false} modules={[Pagination, Autoplay]} breakpoints={{100: {slidesPerView: 1}}}>
-                    <SwiperSlide className="client">
-                        <p className="comments">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum magnam quasi impedit placeat doloribus consequuntur sapiente totam nisi ut assumenda, modi rem vitae quidem laudantium qui obcaecati maxime voluptatibus possimus.</p>
-                        <div className="profile">
-                            <div className="profile-info">
-                                <img src={profile1} alt="" className="comment-img" />
-                                <div className="profile-text">
-                                    <h3>Mrs Someone</h3>
-                                    <p>Weight Loss Theraphy</p>
+            <Swiper className="testimonials-container mySwiper" slidesPerView={2} loop={true} grabCursor={true} 
+                spaceBetween={24} autoplay={{ delay: 1500, disableOnInteraction: false }} pagination={{ clickable: true }} 
+                breakpoints={{ 360: { slidesPerView: 1 }, 480: { slidesPerView: 1 }, 576: { slidesPerView: 1 }, 786: { slidesPerView: 1, spaceBetween: 48 }}} 
+                modules={ [Pagination, Autoplay] }>
+                {
+                Data.map(({
+                    id,
+                    image,
+                    title,
+                    description,
+                    program
+                }) => {
+                    return (
+                        <SwiperSlide className="testimonials-card" key={id}>
+                            <p className="testimonial-desription"> {description}</p>
+                            <div className="text">
+                                <img src={image}className="testimonial-image"  alt=""/>
+                                <div className="testimonial-name">
+                                    {title}
+                                    {program}
                                 </div>
-                            </div>
-                            <div className="icon">
                                 <i className="fa-solid fa-quote-left"></i>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide  className="client">
-                        <p className="comments">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum magnam quasi impedit placeat doloribus consequuntur sapiente totam nisi ut assumenda, modi rem vitae quidem laudantium qui obcaecati maxime voluptatibus possimus.</p>
-                        <div className="profile">
-                            <div className="profile-info">
-                                <img src={profile2} alt="" className="comment-img" />
-                                <div className="profile-text">
-                                    <h3>Mrs Someone</h3>
-                                    <p>Weight Loss Theraphy</p>
-                                </div>
-                            </div>
-                            <div className="icon">
-                                <i className="fa-solid fa-quote-left"></i>
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className="client">
-                        <p className="comments">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum magnam quasi impedit placeat doloribus consequuntur sapiente totam nisi ut assumenda, modi rem vitae quidem laudantium qui obcaecati maxime voluptatibus possimus.</p>
-                        <div className="profile">
-                            <div className="profile-info">
-                                <img src={profile3} alt="" className="comment-img" />
-                                <div className="profile-text">
-                                    <h3>Mrs Someone</h3>
-                                    <p>Weight Loss Theraphy</p>
-                                </div>
-                            </div>
-                            <div className="icon">
-                                <i className="fa-solid fa-quote-left"></i>
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className="client">
-                        <p className="comments">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum magnam quasi impedit placeat doloribus consequuntur sapiente totam nisi ut assumenda, modi rem vitae quidem laudantium qui obcaecati maxime voluptatibus possimus.</p>
-                        <div className="profile">
-                            <div className="profile-info">
-                                <img src={profile1} alt="" className="comment-img" />
-                                <div className="profile-text">
-                                    <h3>Mrs Someone</h3>
-                                    <p>Weight Loss Theraphy</p>
-                                </div>
-                            </div>
-                            <div className="icon">
-                                <i className="fa-solid fa-quote-left"></i>
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                </Swiper>
-            </div>
-            {/* <div className="progress">
-                <div className="text">
-                    <h3>Progress</h3>
-                    <p className="title">Our Client Transformations</p>
-                </div>
-                <div className="transformations">
-                    <div className="transformation">
-                        <div className="before">
-                            <h5 className="before-title">Before</h5>
-                            <img src={imgBefore1} alt="" />
-                        </div>
-                        <div className="after">
-                            <h5 className="after-title">After</h5>
-                            <img src={imgAfter1} alt="" />
-                        </div>
-                    </div>
-                    <div className="transformation">
-                        <div className="before">
-                            <h5 className="before-title">Before</h5>
-                            <img src={imgBefore2} alt="" />
-                        </div>
-                        <div className="after">
-                            <h5 className="after-title">After</h5>
-                            <img src={imgAfter2} alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div> */}
+                        </SwiperSlide>
+                    )
+                })
+            } </Swiper>
         </section>
     )
 }
